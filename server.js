92f8435aa53c70f2d3fe5dfa945bcd462ff41813
aakash2060 <aakash.poudel.900@gmail.com>
2025-01-30 14:13:52 -0600

    gen folder along with ruskin.xpr added

--- server.js
+++ server.js
@@ -136,7 +136,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9001;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
