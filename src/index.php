5d4fa03cfcfd91213b6afe3b89587ce2745de321
Subeen9 <subin.bista@selu.edu>
2025-03-19 18:54:39 -0500

    Base to new server

--- src/index.php
+++ src/index.php
@@ -1,28 +1,81 @@
-<?php include_once('header.inc.php'); ?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title>The Early Ruskin Manuscripts, 1826&#x2013;1842</title>
-	<link href="<?php echo r_build_url("css/styles.css"); ?>" rel="stylesheet" type="text/css" />
-	<link rel="icon"
-	type="image/png"
-	href="images/ruskin_icon.png">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="http://js.arcgis.com/3.14/esri/css/esri.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <script src="http://js.arcgis.com/3.14/"></script>
+    <link rel = "stylesheet" href="style.css">
 
 
 </head>
-<body>
-    <?php include("navigation.inc.php"); ?>
-    <div class="content">
-        <div class="page-title">The Early Ruskin Manuscripts, 1826&#x2013;1842</div>
-        <p><i>The Early Ruskin Manuscripts, 1826&#x2013;1842</i> is an archive of the youthful writing of John Ruskin (1819&#x2013;1900). When complete, <i>ERM</i> will present a comprehensive diplomatic edition of Ruskin&#x02BC;s early writing paired with facsimiles of the manuscripts. The documentary record of this early writing is distinguished by its extraordinary quality and range of topics, including poetry, science, religion, mathematics, education, travel writing, and other disciplines. <i>ERM</i> provides a substantial resource for study of an early Victorian British education.</p>
-        <p>The purview of <i>ERM</i> is not only the late 1820s through the early 1840s, when Ruskin was developing as a writer and coming forward as a pubished author, but also the 1890s through the turn of the century when a second generation of his editors sought to represent and convey the corpus of juvenilia and youthful writing by a major &#x201C;modern&#x201D; (i.e., nineteenth&#x2010;century) author. <i>ERM</i> is therefore designed to interest scholars of Victorian culture generally as well as students of Ruskin in particular.</p>
-        <p>Note: while most of the design and functionality of <i>ERM</i> is complete, we continue to work to optimize the site across browsers and devices and to add new manuscripts, works, and editorial commentary to the archive.</p>
-        <br/>
-        <span class="head-section">News</span>
-        <br/>
-		<p>2.19.15&#x2014;<i>ERM</i> debuts at the Ruskin Seminar, Lancaster University.</p>
+<header>
+    <div id="header" style="display: block; background-color: #3B5786;">
+        <div class="rightArea">
+            <div class="logo">
+                <a target="_blank" style="cursor: default;">
+                    <img alt="Map Tour Logo" class="headerLogoImg" src="http://i446.photobucket.com/albums/qq183/coryclapp/ruskin_logo_zpsz32unfpj.jpg">
+                </a>
+            </div>
+        </div>
+        <div class="textArea" style="width: 829px;">
+            <h1 class="title" tabindex="0" style="margin-top: 40px;">Ruskins Journey</h1>
+            <h2 class="subtitle" tabindex="-1" style="height: 32px;"></h2>
+        </div>
     </div>
+</header>
+
+<body>
+<div id="wrap">
+
+    <div id="main" class="clearfix">
+
+    </div>
+
+</div>
+
+<div id="footer">
+<div class="toggle"><i class="fa fa-arrow-up"></i></div>
+    <div class= "timeline">
+	<?php
+$mysqli = new mysqli("localhost", "root", "", "maps");
+
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+$query = "SELECT * FROM `location`";
+
+if ($result = $mysqli->query($query)) {
+
+    /* fetch associative array */
+    while ($row = $result->fetch_assoc()) {
+        printf ("%s (%s)\n", $row["name"], $row["date"]);
+    }
+
+    /* free result set */
+    $result->free();
+}
+
+/* close connection */
+$mysqli->close();
+
+?>
+
+	</div>
+</div>
+
+
+<div id="mapDiv"></div>
+<script src="script.js"></script>
+<script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+<script>
+	$("#footer").click(function(){
+		$(this).toggleClass("opened")
+	})
+</script>
 </body>
 </html>
